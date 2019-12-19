3d5bfa0e860ab40ea10ee643fd4dd912d5827f44
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 10:13:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.reverse
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
 end
-
-reverse_each_word.each do 
